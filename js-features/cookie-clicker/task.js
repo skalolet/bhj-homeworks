d71b5011274ea58c5cf026639eef9f8b9de511db
skalolet <skalolet@gmail.com>
2023-07-13 17:08:51 +0300

    task.js changed (height / width added)

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,8 +1,7 @@
 const cookie = document.getElementById("cookie");
 const clickerCounter = document.getElementById("clicker__counter");
-let counter = 0;
 
-cookie.addEventListener("click", function() {
-    counter++;
-    clickerCounter.textContent = counter;
-});
+cookie.onclick = () => {
+    let clicks = ++clickerCounter.textContent;
+    cookie.width = clicks % 2 === 0 ? 200 : 300;
+};
